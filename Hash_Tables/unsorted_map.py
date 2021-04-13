a6f70b638e2709908acf4e62aab94d2eed06cc59
AJ <amarjits1@hotmail.com>
2021-04-12 21:07:47 -0600

    wrote getter and setter

--- Hash_Tables/unsorted_map.py
+++ Hash_Tables/unsorted_map.py
@@ -3,7 +3,6 @@
 Created By: AJ Singh
 Date: April 12, 2021
 """
-from typing import Iterator, _T_co, _KT, _VT_co, _VT
 
 from map_abc import MapBase
 
@@ -12,19 +11,42 @@ class UnsortedMap(MapBase):
     """Map implementation using an unordered list."""
 
     def __init__(self):
+        """Create an empty map."""
+        self._table = list()
+
+    def __setitem__(self, k, v):
+        """Assign value v to key k, or re-assign if key k already exists."""
+
+        for item in self._table:  # Check if key k exists.
+            if item._key == k:
+                item._value = v
+                return
+
+        new_item = self._Item(k, v)  # Add key-value pair if key doesn't exist.
+        self._table.append(new_item)
+
+    def __getitem__(self, k):
+        """Return value associated with key k; raise KeyError if does not exist."""
+
+        for item in self._table:
+            if item._key == k:
+                return item._value
+        else:
+            raise KeyError("Key {0} does not exist in map.".format(k))
+
+    def __delitem__(self, v):
         pass
 
-    def __getitem__(self, k: _KT) -> _VT_co:
+    def __len__(self):
         pass
 
-    def __setitem__(self, k: _KT, v: _VT) -> None:
+    def __iter__(self):
         pass
 
-    def __delitem__(self, v: _KT) -> None:
-        pass
 
-    def __len__(self) -> int:
-        pass
+if __name__ == "__main__":
 
-    def __iter__(self) -> Iterator[_T_co]:
-        pass
+    x = UnsortedMap()
+    x["a"] = 1
+    print(x["a"])
+    x["b"]
