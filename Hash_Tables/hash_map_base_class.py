36f1ea348b2cc27728e0096ca991c466771325ad
AJ <amarjits1@hotmail.com>
2021-04-15 16:57:51 -0600

    wrote __init__

--- Hash_Tables/hash_map_base_class.py
+++ Hash_Tables/hash_map_base_class.py
@@ -5,14 +5,20 @@ Created By: AJ Singh
 Date: April 15, 2021
 """
 
+from random import randrange
 from map_abc import MapBase
 
 
 class HashMapBase(MapBase):
     """ABC for a map implemented with a hash table, using MAD compression."""
 
-    def __init__(self):
-        pass
+    def __init__(self, capacity=11, prime=109345121):
+        """Initializes a new empty hash table."""
+        self._table = [None] * capacity
+        self._size = 0                        # Number of unique entries in map.
+        self._prime = prime                   # Prime number needed for MAD compression.
+        self._scale = 1 + randrange(prime-1)  # Scale needed for MAD.
+        self._shift = randrange(prime)        # Shift needed for MAD.
 
     def _hash_function(self):
         pass
