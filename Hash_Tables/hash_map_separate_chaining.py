66230e25e2b5dca212141dc855fa106cd627c42c
AJ <amarjits1@hotmail.com>
2021-04-16 16:05:07 -0600

    set up abstract methods to write

---
+++ Hash_Tables/hash_map_separate_chaining.py
@@ -0,0 +1,24 @@
+"""Implementing a hash map using separate chaining for collision handling.
+
+Created By: AJ Singh
+Date: April 16, 2021
+"""
+
+from unsorted_map import UnsortedMap
+from hash_map_base_class import HashMapBase
+
+
+class ChainHashMap(HashMapBase):
+    """A hash map implementation using separate chaining for collision handling."""
+
+    def _bucket_setitem(self, j, k, v):
+        pass
+
+    def _bucket_getitem(self, j, k):
+        pass
+
+    def _bucket_delitem(self, j, k):
+        pass
+
+    def __iter__(self):
+        pass
