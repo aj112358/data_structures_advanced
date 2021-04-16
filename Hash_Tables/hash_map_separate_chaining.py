953abfb2686316aac7ab2e16ef6c3c4e982518ac
AJ <amarjits1@hotmail.com>
2021-04-16 16:20:03 -0600

    implemented concrete setitem method

--- Hash_Tables/hash_map_separate_chaining.py
+++ Hash_Tables/hash_map_separate_chaining.py
@@ -1,4 +1,4 @@
-"""Implementing a hash map using separate chaining for collision handling.
+"""Implementing a hash map using separate chaining for collision handling, taken from book.
 
 Created By: AJ Singh
 Date: April 16, 2021
@@ -12,7 +12,19 @@ class ChainHashMap(HashMapBase):
     """A hash map implementation using separate chaining for collision handling."""
 
     def _bucket_setitem(self, j, k, v):
-        pass
+
+        # Check if hash table at key k currently has a container.
+        # If not, create a new container, which is simply an unsorted map (collection of _Item objects).
+        if self._table[j] is None:
+            self._table[j] = UnsortedMap()
+
+        oldsize = len(self._table[j])
+
+        self._table[j][k] = v
+
+        # Increment size if (k,v) element is new element (not just overwriting existing value).
+        if len(self._table[j]) > oldsize:
+            self._size += 1  # Increment number of unique entries in map.
 
     def _bucket_getitem(self, j, k):
         pass
