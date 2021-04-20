06b1be41c98e39d8e9d79a23ef4e223a071e0caf
AJ <amarjits1@hotmail.com>
2021-04-20 17:54:17 -0600

    wrote __init__ and add methods

--- Hash_Tables/multimap.py
+++ Hash_Tables/multimap.py
@@ -8,16 +8,20 @@ Date: April 20, 2021
 class MultiMap:
     """A multimap class built upon use of an underlying map for storage."""
 
-    _MapType = dict
+    _MapType = dict  # Data structure to use to contain elements; Can be overridden by subclassing.
 
     def __init__(self):
-        pass
-
-    def __iter__(self):
-        pass
+        """Create a new empty multimap instance."""
+        self._map = self._MapType()
+        self._n = 0
 
     def add(self, k, v):
-        pass
+        """Add pair (k,v) to multimap."""
+        if k not in self._map:
+            self._map[k] = list(v)
+        else:
+            self._map[k].append(v)
+        self._n += 1
 
     def pop(self, k):
         pass
@@ -26,4 +30,7 @@ class MultiMap:
         pass
 
     def find_all(self, k):
+        pass
+
+    def __iter__(self):
         pass
