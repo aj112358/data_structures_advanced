a740d2e3d8252052c87db43cb7da86c8df7dd45c
AJ <amarjits1@hotmail.com>
2021-04-20 17:50:24 -0600

    setting up methods for multimap class

---
+++ Hash_Tables/multimap.py
@@ -0,0 +1,29 @@
+"""An implementation of a multi-set (can contain duplicate elements).
+
+Created By: AJ Singh
+Date: April 20, 2021
+"""
+
+
+class MultiMap:
+    """A multimap class built upon use of an underlying map for storage."""
+
+    _MapType = dict
+
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def add(self, k, v):
+        pass
+
+    def pop(self, k):
+        pass
+
+    def find(self, k):
+        pass
+
+    def find_all(self, k):
+        pass
