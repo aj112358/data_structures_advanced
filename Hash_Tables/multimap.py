7c55bce00ac6153418752e73635651a18c37abb3
AJ <amarjits1@hotmail.com>
2021-04-20 18:04:46 -0600

    wrote pop() method to return random value

--- Hash_Tables/multimap.py
+++ Hash_Tables/multimap.py
@@ -13,7 +13,7 @@ class MultiMap:
     def __init__(self):
         """Create a new empty multimap instance."""
         self._map = self._MapType()
-        self._n = 0
+        self._n = 0  # Total number of elements in set (including duplicates)
 
     def add(self, k, v):
         """Add pair (k,v) to multimap."""
@@ -26,7 +26,18 @@ class MultiMap:
         self._n += 1
 
     def pop(self, k):
-        pass
+        """Remove and return an arbitrary (k,v) item, or raise KeyError if set is empty or key k does not exist."""
+        if self._n == 0:
+            raise KeyError("Set is empty.")
+
+        container = self._map[k]  # Will raise KeyError if k not in set. Gets all values associated with key k.
+        v = container.pop()       # Return one random value.
+
+        if len(container) == 0:   # Check if key has any remaining values.
+            del self._map[k]      # If not, can remove key from set.
+        self._n -= 1
+        return k, v
+
 
     def find(self, k):
         pass
