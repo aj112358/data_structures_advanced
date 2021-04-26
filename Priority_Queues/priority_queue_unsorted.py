3cf6698853017c93a63b7fca65613f03aff48043
AJ <amarjits1@hotmail.com>
2021-04-25 18:55:47 -0600

    setting up class methods

---
+++ Priority_Queues/priority_queue_unsorted.py
@@ -0,0 +1,31 @@
+"""Implementing a priority queue using an *unsorted* positional list.
+
+Created By: AJ Singh
+Date: April 25, 2021
+"""
+
+from Linked_Lists.positional_list import PositionalList
+from priority_queue_base_class import PriorityQueueBase
+
+
+class UnsortedPriorityQueue(PriorityQueueBase):
+    """A minimum-oriented priority queue implemented with an unsorted positional list."""
+
+    # Non-Public Utility Method
+    def _find_min(self):
+        pass
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def add(self, key, value):
+        pass
+
+    def min(self):
+        pass
+
+    def remove_min(self):
+        pass
