96f328f2ccef8204d413a25ccd1676c62ccc4637
AJ <amarjits1@hotmail.com>
2021-04-25 19:41:17 -0600

    setting up class methods

---
+++ Priority_Queues/priority_queue_sorted.py
@@ -0,0 +1,27 @@
+"""Implementing a sorted priority queue using a positional list.
+
+Created By: AJ Singh
+Date: April 25, 2021
+"""
+
+from priority_queue_base_class import PriorityQueueBase
+from Linked_Lists.positional_list import PositionalList
+
+
+class SortedPriorityQueue(PriorityQueueBase):
+    """A minimum-oriented priority queue implementation with a sorted positional list."""
+
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def add(self, key, value):
+        pass
+
+    def min(self):
+        pass
+
+    def remove_min(self):
+        pass
