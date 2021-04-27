73ade14d5eb68c6e969e6418a63ce4625026ad10
AJ <amarjits1@hotmail.com>
2021-04-27 15:37:27 -0600

    wrote up-heap and down-heap bubbling code

--- Priority_Queues/heap_priority_queue.py
+++ Priority_Queues/heap_priority_queue.py
@@ -27,14 +27,32 @@ class HeapPriorityQueue(PriorityQueueBase):
     def _has_right(self, j):
         return self._right(j) < len(self._data)
 
-    def _swap(self):
-        pass
+    def _swap(self, i, j):
+        """Swap the elements at indices i and j of array."""
+        self._data[i], self._data[j] = self._data[j], self._data[i]
 
-    def _upheap(self):
-        pass
+    def _upheap(self, j):
+        parent = self._parent(j)
+        if j > 0 and self._data[j] < self._data[parent]:
+            self._swap(j, parent)
+            self._upheap(parent)
 
-    def _downheap(self):
-        pass
+    def _downheap(self, j):
+        if self._has_left(j):
+            left = self._left(j)
+            small_child = left
+
+            if self._has_right(j):
+                right = self._right(j)
+
+                if self._data[right] < self._data[left]:  # Determine which of left and right child is smaller.
+                    small_child = right
+                # else:
+                #     small_child = left
+
+            if self._data[small_child] < self._data[j]:
+                self._swap(j, small_child)
+                self._downheap(small_child)
 
     # ----- Public ADT Methods ----- #
 
